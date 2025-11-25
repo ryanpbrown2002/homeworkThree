f7cfab10ebebb9f3e2dbde7d1b7531760562bd7c
Ryan B <ryanpbrown2002@gmail.com>
2025-11-25 23:54:35 +0000

    Initalize pdf discovery & validation modules

---
+++ modules/pdfDiscovery.js
@@ -0,0 +1,8 @@
+// modules/pdfDiscovery.js
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * PDF Discovery Module
+ * Searches for available PDF documents in the designated folder
+ */
---
+++ modules/pdfValidation.js
@@ -0,0 +1,8 @@
+// modules/pdfValidation.js
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * PDF Validation Module
+ * Checks if requested PDF documents exist and are within the designated folder
+ */
