df61c25cb83cf84369c7802270454036ecb3694b
Ryan B <ryanpbrown2002@gmail.com>
2025-12-07 01:18:44 +0000

    add / & /pdfs routes

--- backend/modules/router.js
+++ backend/modules/router.js
@@ -1,3 +1,15 @@
 // modules/router.js
 const path = require('path');
 
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../nginx/public/index.html'));
+});
+
+router.get('/pdfs', (req, res) => {
+    res.sendFile(path.join(__dirname, '../nginx/public/pdfs.html'));
+});
+
+
+module.exports = router;
