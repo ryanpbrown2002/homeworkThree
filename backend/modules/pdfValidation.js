a0bdc4f4a2e53c7be70dee2c34624120fc42066b
Ryan B <ryanpbrown2002@gmail.com>
2025-12-07 02:19:35 +0000

    Create pdf validation module

--- backend/modules/pdfValidation.js
+++ backend/modules/pdfValidation.js
@@ -1,3 +1,11 @@
 // modules/pdfValidation.js
 const fs = require('fs');
 const path = require('path');
+
+// Check if pdf document actually exists before serving it to user
+function pdfValidation(filename) {
+    return fs.existsSync(path.join(__dirname, '../pdfs', filename));
+}
+
+// export pdfValidation
+module.exports = pdfValidation;
