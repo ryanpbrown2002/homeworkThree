48edc3f277329d8555d602036e837ac03dd0e775
Ryan B <ryanpbrown2002@gmail.com>
2025-12-07 21:10:54 +0000

    add comments to server.js

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,13 @@ const router = require('./modules/router');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// use router
 app.use(router);
 
+// sync database upon startup
 db.syncDatabase();
 
+// start server
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
